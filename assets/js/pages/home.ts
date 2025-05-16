3ea09e0820c380c964b91fc899aa124047ff4266
Moyaxzan <tao.spa2003@gmail.com>
2025-05-16 18:10:15 +0200

    changed head/foot colors according to figma + started front for the Pong

--- assets/js/pages/home.ts
+++ assets/js/pages/home.ts
@@ -1,9 +1,11 @@
 export function renderHome() {
     const app = document.getElementById('app')!;
     app.innerHTML = `
-      <div class="p-8">
-        <h1 class="text-3xl font-bold text-blue-600">Home Page</h1>
-      </div>
+    <div class="p-8">
+    	<h1 class="text-3xl font-bold text-blue-600">Home Page</h1>
+    </div>
+	<button id="userLoad">Display users</button>
+	<div id="userList" style="margin-top: 10px;"></div>
     `;
   }
   
--- assets/js/pages/pong.ts
+++ assets/js/pages/pong.ts
@@ -1,8 +1,10 @@
-export function renderPong() {
-    const app = document.getElementById('app')!;
-    app.innerHTML = `
-      <div class="p-8">
-        <h1 class="text-3xl font-bold text-blue-600">this is the pong page</h1>
-      </div>
-    `;
-  }
+export async function renderPong() {
+	const app = document.getElementById('app');
+	if (!app)
+		return;
+
+	const res = await fetch('/dist/js/pages/pong.html');
+	const html = await res.text();
+
+	app.innerHTML = html;
+}
