8746807b703aa4123f9f6bc70a7235fee71b9e6b
Elsa TOUMA <eltouma@f6r12s4.clusters.42paris.fr>
2025-05-16 18:48:08 +0200

    add pong container

---
+++ srcs/requirements/pong/server.js
@@ -0,0 +1,28 @@
+// ESM
+import Fastify from 'fastify'
+//import fastifySqlite from 'fastify-sqlite'
+//import dbConnector from './db_connection/db.js'
+import routes from './routes/route.js'
+
+const fastify = Fastify({
+	logger: true
+})
+
+/*
+fastify.register(fastifySqlite, {
+	dbFile: './data/transcendence_db.db'
+})
+
+
+fastify.register(dbConnector)
+*/
+fastify.register(routes)
+
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+	if (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	} else {
+		fastify.log.info(`server is listening on ${address}`)
+	}
+})
