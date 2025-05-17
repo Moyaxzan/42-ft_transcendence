2f90b44147309dc71b7c36310b30bd8266d2d66a
User <user@mbp-de-user.home>
2025-05-18 00:01:47 +0200

    Change glow

--- assets/js/pages/user.ts
+++ assets/js/pages/user.ts
@@ -1,8 +1,6 @@
 export function renderUser() {
 	const app = document.getElementById("userLoad")!;
- app.innerHTML = `
-	<p><span>Display users</span></p>
-  `;
+ app.innerHTML = `<span>Display users</span>`;
 console.log("app div found?", app);
 }
   
