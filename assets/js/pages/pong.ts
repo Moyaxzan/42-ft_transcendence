e5ba48726094946285e1c19ee590364436e2dfa1
Moyaxzan <tao.spa2003@gmail.com>
2025-05-20 17:13:44 +0200

    paddles of pong game are now moving

--- assets/js/pages/pong.ts
+++ assets/js/pages/pong.ts
@@ -7,4 +7,56 @@ export async function renderPong() {
 	const html = await res.text();
 
 	app.innerHTML = html;
+
+
+
+
+
+
+	// Paddle movement setup
+	let leftPaddle = document.getElementById("left-paddle") as HTMLDivElement;
+	let rightPaddle = document.getElementById("right-paddle") as HTMLDivElement;
+	if (!leftPaddle || !rightPaddle) return;
+
+
+	let keysPressed: {[key: string] : boolean} = {};
+
+	document.addEventListener("keydown", (e) => {
+		keysPressed[e.key] = true;
+	});
+
+	document.addEventListener("keyup", (e) => {
+		keysPressed[e.key] = false;
+	});
+
+	let leftPaddlePos = 0;   // as %
+	let rightPaddlePos = 0;  // as %
+
+	function movePaddles() {
+		const speed = 0.65;     // % per key press
+
+		if ((keysPressed["s"] || keysPressed["S"]) && !(keysPressed["w"] || keysPressed["W"])) {
+			leftPaddlePos += speed;
+		}
+		else if (keysPressed["w"] || keysPressed["W"] && !(keysPressed["s"] || keysPressed["S"])) {
+			leftPaddlePos -= speed;
+		}
+		if (keysPressed["ArrowUp"] && !keysPressed["ArrowDown"]) {
+			rightPaddlePos -= speed;
+		}
+		else if (keysPressed["ArrowDown"] && !keysPressed["ArrowUp"]) {
+			rightPaddlePos += speed;
+		}
+		leftPaddlePos = Math.max(0, Math.min(82, leftPaddlePos));
+		rightPaddlePos = Math.max(0, Math.min(82, rightPaddlePos));
+
+		leftPaddle.style.top = `${leftPaddlePos}%`;
+		rightPaddle.style.top = `${rightPaddlePos}%`;
+		requestAnimationFrame(movePaddles)
+	}
+
+
+	requestAnimationFrame(movePaddles)
 }
+
+
--- srcs/requirements/front/dist/js/pages/pong.js
+++ srcs/requirements/front/dist/js/pages/pong.js
@@ -15,5 +15,40 @@ export function renderPong() {
         const res = yield fetch('/dist/html/pong.html');
         const html = yield res.text();
         app.innerHTML = html;
+        // Paddle movement setup
+        let leftPaddle = document.getElementById("left-paddle");
+        let rightPaddle = document.getElementById("right-paddle");
+        if (!leftPaddle || !rightPaddle)
+            return;
+        let keysPressed = {};
+        document.addEventListener("keydown", (e) => {
+            keysPressed[e.key] = true;
+        });
+        document.addEventListener("keyup", (e) => {
+            keysPressed[e.key] = false;
+        });
+        let leftPaddlePos = 0; // as %
+        let rightPaddlePos = 0; // as %
+        function movePaddles() {
+            const speed = 0.65; // % per key press
+            if ((keysPressed["s"] || keysPressed["S"]) && !(keysPressed["w"] || keysPressed["W"])) {
+                leftPaddlePos += speed;
+            }
+            else if (keysPressed["w"] || keysPressed["W"] && !(keysPressed["s"] || keysPressed["S"])) {
+                leftPaddlePos -= speed;
+            }
+            if (keysPressed["ArrowUp"] && !keysPressed["ArrowDown"]) {
+                rightPaddlePos -= speed;
+            }
+            else if (keysPressed["ArrowDown"] && !keysPressed["ArrowUp"]) {
+                rightPaddlePos += speed;
+            }
+            leftPaddlePos = Math.max(0, Math.min(82, leftPaddlePos));
+            rightPaddlePos = Math.max(0, Math.min(82, rightPaddlePos));
+            leftPaddle.style.top = `${leftPaddlePos}%`;
+            rightPaddle.style.top = `${rightPaddlePos}%`;
+            requestAnimationFrame(movePaddles);
+        }
+        requestAnimationFrame(movePaddles);
     });
 }
